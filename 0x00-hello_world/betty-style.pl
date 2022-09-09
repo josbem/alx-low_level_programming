61788a83af4fb896d4a148d39b9bf123deb74613
josbem <orinajosbem@gmail.com>
2022-09-08 18:06:51 -0700

    C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/betty-style.pl
+++ 0x00-hello_world/betty-style.pl
@@ -6,9 +6,12 @@
         * Description: using the main function
         * this program prints "Programming is like building a multilingual puzzle"
         * Return: 0
+	* Description return value of main: returns 0 since their are no parameters on function main
         */
         int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
